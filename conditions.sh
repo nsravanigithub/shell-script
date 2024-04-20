ecd01752203780ef25a4b3878ddd3a09130ac81f
nsravanigithub <nsravani1287@gmail.com>
2024-04-20 19:28:30 +0530

    variables update

---
+++ conditions.sh
@@ -0,0 +1,15 @@
+ #!/bin/bash
+
+ #using if condition to find whether given number is greater than or equal to 10
+
+ NUMBER=$1
+ if [ $NUMBER > 10 ]
+ then
+ echo "Given number is greater than 10"
+ else
+ echo "Given number is less than 10"
+ fi
+
+ #-gt, -lt, -eq, -ge, le is also used for greater than
+ #less than,equal to,greater than equal to,less than equal to
+

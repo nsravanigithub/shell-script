6176b436c74c9112062c836dc44e115666465770
nsravanigithub <nsravani1287@gmail.com>
2024-04-20 17:01:00 +0530

    variables update

---
+++ variables2.sh
@@ -0,0 +1,9 @@
+#declaring the variables
+person1=$1
+person2=$2
+
+#referring the variables
+echo "$person1--->Hello $person2, how are you?"
+echo "$person2--->I am fine $person1, how are you?"
+echo "$person1--->I am fine too...where are you now?"
+echo "$person2--->I am at my home. Don't you know that?"

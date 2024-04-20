4e76326ceac3f0c16a3224ba19802d6f1c30ed1e
nsravanigithub <nsravani1287@gmail.com>
2024-04-20 17:28:26 +0530

    variables update

--- array1.sh
+++ array1.sh
@@ -5,9 +5,9 @@ movies=("RRR" "WRONG TURN" "HOLIDAY" "PURSUIT OF HAPPINESS")
 #size of the array is 4
 #index takes it as 3 that means it will start from 0,1,2,3
 
-echo "I want to watch the movie ${movie[0]}"
-echo "I want to watch the movie ${movie[1]}"
-echo "I want to watch the movie ${movie[2]}"
-echo "I want to watch the movie ${movie[@]}"
+echo "I want to watch the movie ${movies[0]}"
+echo "I want to watch the movie ${movies[1]}"
+echo "I want to watch the movie ${movies[2]}"
+echo "I want to watch the movie ${movies[@]}"
 
 #@--->it will give all the list in the array

c7cfe9ef2bd393a159f13b7e2dce25143a0382e4
nsravanigithub <nsravani1287@gmail.com>
2024-04-20 17:12:52 +0530

    variables update

---
+++ variables3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Please enter username:"
+
+read username
+
+echo "Please enter password:"
+
+read password
+
+echo "username is: $username , password is: $password"
+
+#read -s username (if u give -s before username,u cannot see the username that u are r typing)
+ 

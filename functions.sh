9440e48edd328a507097a64ee0a2a81ec4769c56
nsravanigithub <nsravani1287@gmail.com>
2024-04-21 12:19:25 +0530

    testing

---
+++ functions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+user=$(id -u)
+#validating root user
+
+validate()
+{
+    if [ $? -ne 0 ]
+    then
+    echo "run with root access.. $1"
+    exit 1
+    else
+    echo "you are super user... $2"
+    fi
+}
+
+dnf install mysql -y
+
+dnf install git -y

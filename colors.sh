0cd4358dfa231783b6492e81c8880b56ec36f7fe
nsravanigithub <nsravani1287@gmail.com>
2024-04-21 15:35:29 +0530

    testing

--- colors.sh
+++ colors.sh
@@ -11,10 +11,10 @@ validate()
 {
     if [ $1 -ne 0 ]
     then
-    echo -e("$2........$R failure")
+    echo -e ("$2........$R failure")
     exit 1
     else
-    echo -e("$2.....$G Success")
+    echo -e ("$2.....$G Success")
     fi
 
 }

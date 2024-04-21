25c352ebcfcb9e9c02fe575dd3d2120977cc6933
nsravanigithub <nsravani1287@gmail.com>
2024-04-21 15:33:43 +0530

    testing

---
+++ colors.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+user=$(id -u)
+#validating root user
+Time=$(date +%Y.%m.%d-%H.%M.%S)
+scriptname=$( echo $0 | cut -d "." -f1) #it cuts the .sh from script name 
+logfile=/tmp/$scriptname.$Time.log
+R=$(\e[31])
+G=$(\e[32])
+validate()
+{
+    if [ $1 -ne 0 ]
+    then
+    echo -e("$2........$R failure")
+    exit 1
+    else
+    echo -e("$2.....$G Success")
+    fi
+
+}
+    if [ $? -ne 0 ]
+    then
+    echo "Please run with root access"
+    exit 1
+    else
+    echo "you are super user"
+    fi
+
+
+dnf install mysql -y &>> $logfile  #it will append whatever output u get into the logfilecd
+
+validate $? "installing mysql" 
+dnf install git -y &>> $logfile
+
+validate $? "installing git"
